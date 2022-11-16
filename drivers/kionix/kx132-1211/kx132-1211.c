e1d043a353775c50cc1011b85c1c0d5e5cfeb6c0
Ted Havelka <tedhavelka66@gmail.com>
2022-11-15 16:07:53 -0800

    WIP - double quotes still getting doubled, and intermediate macro string too long per device.h

--- drivers/kionix/kx132-1211/kx132-1211.c
+++ drivers/kionix/kx132-1211/kx132-1211.c
@@ -663,7 +663,10 @@ DEVICE_DT_DEFINE(
 //                 DT_NODELABEL(CONFIG_KX132_NODELABEL_VALUE),  // node_id . . . works, but nodelabel is hard-coded
 // [app_workspace]/zephyr/include/zephyr/devicetree.h:190:36: error: pasting "DT_N_NODELABEL_" and ""kionix_sensor"" does not give a valid preprocessing token
 
-                 DT_NODELABEL(str(CONFIG_KX132_NODELABEL_VALUE)),  // node_id . . . works, but nodelabel is hard-coded
+//                 DT_NODELABEL(str(CONFIG_KX132_NODELABEL_VALUE)),  // node_id . . . works, but nodelabel is hard-coded
+
+                 DT_NODELABEL(str(KXNL)),      // node_id . . . works, but nodelabel is hard-coded
+
 
                  kx132_1211_init,              // init function
                  NULL,                         // pm
