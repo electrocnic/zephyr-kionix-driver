bd227114b6ea61ceb3f1d9dcdeb6c3ea14118930
Ted Havelka <thavelka@cornellpump.com>
2022-11-23 14:30:16 -0800

    Adding missed new file.

---
+++ drivers/kionix/kx132-1211/kx132-triggers.c
@@ -0,0 +1,37 @@
+
+
+
+
+/**
+ * kx132_trigger_set - link external trigger to event data ready
+ *
+ * NOTE this code copied and adapted from STMicro iis2dh_trigger.c, in Zephyr RTOS 3.2.0.
+ */
+
+int kx132_trigger_set(const struct device *dev,
+		       const struct sensor_trigger *trig,
+		       sensor_trigger_handler_t handler)
+{
+	struct kx132_data *kx132 = dev->data;
+	const struct kx132_device_config *cfg = dev->config;
+//	int16_t raw[3];
+	int state = (handler != NULL) ? PROPERTY_ENABLE : PROPERTY_DISABLE;
+
+	if (!cfg->int_gpio.port) {
+		return -ENOTSUP;
+	}
+
+	switch (trig->type) {
+	case SENSOR_TRIG_DATA_READY:
+		kx132->drdy_handler = handler;
+//		if (state) {
+//			/* dummy read: re-trigger interrupt */
+//			kx132_acceleration_raw_get(kx132->ctx, raw);
+//		}
+		return kx132_enable_drdy(dev, SENSOR_TRIG_DATA_READY, state);
+	default:
+		LOG_ERR("KX132 driver - Unsupported sensor trigger");
+		return -ENOTSUP;
+	}
+}
+
