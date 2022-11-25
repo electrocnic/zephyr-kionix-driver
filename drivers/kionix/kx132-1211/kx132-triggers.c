781aca16bec9d097a00b337b3863afd44f3ddf8f
Ted Havelka <thavelka@cornellpump.com>
2022-11-24 21:40:03 -0800

    WIP - refining diagnostics on cfg->int_gpio.port . . .

--- drivers/kionix/kx132-1211/kx132-triggers.c
+++ drivers/kionix/kx132-1211/kx132-triggers.c
@@ -214,9 +214,13 @@ int kx132_init_interrupt(const struct device *dev)
 //
 
 //        { printk("\n"); }
-//    printk("- MARK 4 -\n");
 
+    printk("- MARK 3 -\n");
+    if ( cfg->int_gpio.port->state != NULL )
+        { printk("- INFO - cfg->int_gpio.port->state not null,\n"; }
+    printk("- MARK 4 -\n");
     printk("- INFO - cfg->int_gpio.port->state->init_res holds %u,\n", cfg->int_gpio.port->state->init_res);
+    printk("- MARK 5 -\n");
 
 
 // DEBUG 1124 END
